a5d013edf4c005b3d591d500195faa1a3e4e79e1
Alamgir Khan <alamgirkhan48692@gmail.com>
2026-07-05 22:35:00 +0500

    initial project version

--- All files/config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = (
-        "mysql+pymysql://root:@localhost/data_analysis"
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- All files/dataset.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-
-dataset = Blueprint("dataset", __name__)
-
-@dataset.route("/dataset")
-def dataset_info():
-    return jsonify({
-        "message": "Dataset API"
-    })
