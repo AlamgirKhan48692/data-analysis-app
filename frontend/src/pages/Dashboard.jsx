862ba88321f8b769b380577fea9dcf6c6834ece0
Alamgir Khan <alamgirkhan48692@gmail.com>
2026-07-06 14:49:14 +0500

    update dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,7 +5,11 @@ function Dashboard(){
         <div className="register-container">
             <div className="register-card">
                 <h1 style={{ color: "green"}}>Welcome to Dashboard</h1>
-                <p style={{textAlign: "center", color: "blue", fontSize: "25px"}}>You are Successfully Loged in</p>
+                <p style={{textAlign: "center", color: "blue", fontSize: "25px"}}>
+                    The dashboard is currently being built. <br />
+                    You can already register, log in, and manage your account. <br />
+                    More features are coming soon.
+                </p>
             </div>
         </div>
     )
