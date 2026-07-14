24fc80fa5e705812e0c2df6c8648d365d55c845a
Alamgir Khan <alamgirkhan48692@gmail.com>
2026-07-14 12:09:42 +0500

    Update Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,17 +1,42 @@
+import { useNavigate } from "react-router-dom";
 
+function Dashboard() {
+  const navigate = useNavigate();
 
-function Dashboard(){
-    return (
-        <div className="register-container">
-            <div className="register-card">
-                <h1 style={{ color: "green"}}>Welcome to Dashboard</h1>
-                <p style={{textAlign: "center", color: "blue", fontSize: "20px"}}>
-                    The dashboard is currently being built. <br />
-                    You can already register, log in, and manage your account. <br />
-                    More features are coming soon.
-                </p>
-            </div>
-        </div>
-    )
+  const username = localStorage.getItem("username");
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("username");
+
+    navigate("/");
+  };
+
+  return (
+    <div className="register-container">
+      <div className="register-card">
+        <h1>Welcome, {username} 👋</h1>
+
+        <p
+          style={{
+            textAlign: "center",
+            fontSize: "18px",
+            color: "#374151",
+            marginBottom: "25px",
+            lineHeight: "1.8",
+          }}
+        >
+          Welcome to your Data Analysis Dashboard.
+          <br />
+          The dashboard is currently under development.
+          <br />
+          More exciting features are coming soon!
+        </p>
+
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    </div>
+  );
 }
-export default Dashboard; 
+
+export default Dashboard;
